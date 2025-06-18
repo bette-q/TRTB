def18be9ea35344605cf7259da0d69f7ee0ffd1b
Betty Qin <bettyqin233@gmail.com>
2025-06-18 16:23:47 -0500

    interactables: evidence + generic

---
+++ Climate Jam 2025/Assets/Script/CharacterSystem/PlayerInteraction.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    public KeyCode interactKey = KeyCode.E;
+    private List<Interactable> nearbyInteractables = new List<Interactable>();
+
+    void Update()
+    {
+        if (Input.GetKeyDown(interactKey) && nearbyInteractables.Count > 0)
+        {
+            // Interact with the closest interactable
+            Interactable closest = null;
+            float closestDist = float.MaxValue;
+            foreach (var interactable in nearbyInteractables)
+            {
+                float dist = (interactable.transform.position - transform.position).sqrMagnitude;
+                if (dist < closestDist)
+                {
+                    closestDist = dist;
+                    closest = interactable;
+                }
+            }
+
+            if (closest != null)
+            {
+                closest.Interact();
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        var interactable = other.GetComponent<Interactable>();
+        if (interactable != null && !nearbyInteractables.Contains(interactable))
+        {
+            nearbyInteractables.Add(interactable);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        var interactable = other.GetComponent<Interactable>();
+        if (interactable != null && nearbyInteractables.Contains(interactable))
+        {
+            nearbyInteractables.Remove(interactable);
+        }
+    }
+}
---
+++ Climate Jam 2025/Assets/Script/Interaction/InteractEvidence.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class InteractEvidence : Interactable
+{
+    public override void Interact()
+    {
+        Debug.LogWarning("Interacted with Evidence!");
+        // TODO: add logic for evidence pickup etc.
+    }
+}

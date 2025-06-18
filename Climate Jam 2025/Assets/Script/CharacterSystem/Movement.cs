6750dac8e5d636370470dc25c202d1119ae4f004
Betty Qin <bettyqin233@gmail.com>
2025-06-18 15:55:12 -0500

    player rigit body + collision

--- Climate Jam 2025/Assets/Script/CharacterSystem/Movement.cs
+++ Climate Jam 2025/Assets/Script/CharacterSystem/Movement.cs
@@ -2,20 +2,21 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float speed = 5.0f;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float speed = 5f;
+    private Rigidbody rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true; // prevents tipping over
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate() // Physics updates go here!
     {
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
-        Vector3 move = new Vector3(h, 0, v).normalized;
-        transform.position += move * speed * Time.deltaTime;
+        Vector3 move = new Vector3(h, 0, v).normalized * speed;
 
+        rb.linearVelocity = move;
     }
 }
